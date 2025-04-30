d81c52b7ab6086247b1f6be4445330f75065ffbc
mateus_britto <mateusbritto00@gmail.com>
2025-04-30 07:49:53 +0200

    Add databases week2 homework

---
+++ databases/week2/homework/part1.sql
@@ -0,0 +1,121 @@
+-- Part 1: Working with tasks
+
+CREATE DATABASE tasks;
+
+USE tasks;
+
+CREATE TABLE task (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP, -- The 'created' column stores the timestamp when a row is first inserted, defaulting to the current time.
+    updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- The 'updated' column stores the timestamp of the last update to the row, automatically updating to the current time whenever the row is modified.
+    due_date DATETIME,
+    status_id INT,
+    user_id INT
+);
+
+-- Insert a task into the task table
+INSERT INTO task (title, description, due_date, status_id, user_id)
+VALUES ('Complete Homework', 'Finish the SQL homework for week 2', '2025-06-01 10:00:00', 1, 101);
+
+SELECT * FROM task;
+
+-- Change the title of a task
+UPDATE task
+SET title = "Workout"
+WHERE id = 1;
+
+-- Change the description of a task
+UPDATE task
+set description = "Go to the Gym"
+WHERE id = 1;
+
+-- Change a task due date
+UPDATE task
+SET due_date = "2025-06-06 12:00:00"
+WHERE id = 1;
+
+-- Change a task status
+UPDATE task
+SET status_id = 2
+WHERE id = 1;
+
+-- Mark a task as complete
+UPDATE task
+SET status_id = 3
+WHERE id = 1;
+
+-- Delete a task
+DELETE from task
+WHERE id = 1;
+
+SELECT * FROM task;
+
+-- DROP DATABASE tasks;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- USE task;
+
+-- -- Create the tasks table
+-- CREATE TABLE tasks (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     title VARCHAR(255) NOT NULL,
+--     description TEXT,
+--     created DATETIME DEFAULT CURRENT_TIMESTAMP,
+--     updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+--     due_date DATETIME,
+--     status_id INT,
+--     user_id INT
+-- );
+
+-- -- Change the title of a task
+-- UPDATE tasks
+-- SET title = 'New Title'
+-- WHERE id = 1;
+
+-- -- Change a task due date
+-- UPDATE tasks
+-- SET due_date = '2023-12-31 23:59:59'
+-- WHERE id = 1;
+
+-- -- Change a task status
+-- UPDATE tasks
+-- SET status_id = 2
+-- WHERE id = 1;
+
+-- -- Mark a task as complete
+-- UPDATE tasks
+-- SET status_id = 3
+-- WHERE id = 1;
+
+-- -- Delete a task
+-- DELETE FROM tasks
+-- WHERE id = 1;
